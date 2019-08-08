be9e3fdaea3ae0eb51f472a810e6adac5740443e
安河内竜二 <ryuji.yasu@gmail.com>
2019-08-08 17:43:19 +0900

    firstcommit

---
+++ frobshop/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.urls import include, path
+from django.apps import apps
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('admin/', admin.site.urls),
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
